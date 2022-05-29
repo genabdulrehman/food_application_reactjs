986ee4c44c3910450a1671f4dd9ae4a7d7377888
genabdulrehman <76816147+genabdulrehman@users.noreply.github.com>
2022-05-29 18:07:23 +0500

    Footer Done

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import SubHeading from '../SubHeading/SubHeading';
 
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className="app__newsletter-heading">
+      <SubHeading title="newsletter" />
+
+      <h1 className='headtext__cormorant'>Subscribe to our newsletter</h1>
+      <p className="p__opensans">never miss new updates!</p>
+
+
+   
+    </div>
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder='Enter Your Email.' />
+      <button className='custom__button'>
+        Subscribe
+      </button>
+    </div>
+   
   </div>
 );
 
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
+import {FooterOverlay, Newsletter} from '../../components';
+import {images} from '../../constants';
+import {FiFacebook, FiTwitter, FiInstagram} from 'react-icons/fi';
 
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className='app__footer section__padding'>
+ <FooterOverlay />
+ <Newsletter  />
+ <div className='app__footer-links'>
+   <div className="app__footer-links_contacts">
+     <h1 className="app__footer-headtext">
+       Contacts
+     </h1>
+
+     <p className="p__opensans">3 Block Road, Ali Town Lahore, Pakistan.</p>
+     <p className="p__opensans">+92 345 43 345</p>
+     <p className="p__opensans">+92 345 43 345</p>
+   </div>
+   <div className="app__footer-links_logo">
+     <img src={images.gericht} alt="footer logo" />
+     <p className="p__opensans">" The best way to find yourself is to loose yourself in others service."</p>
+     <img src={images.spoon} alt="" />
+     <div className="app__footer-links_icons">
+       <FiFacebook />
+       <FiTwitter />
+       <FiInstagram />
+     </div>
+
+   </div>
+   <div className="app__footer-links_work">
+   <h1 className="app__footer-headtext">
+       Working Hours
+     </h1>
+
+     <p className="p__opensans">Monday - Friday :</p>
+     <p className="p__opensans">8:00 am - 5 pm</p>
+     <p className="p__opensans">Saturday - Sunday :</p>
+     <p className="p__opensans">7:00 am - 2 pm</p>
+   </div>
+ </div>
+
+ <div className="footer__copyright">
+   <p className="p__opensans">
+     2022 Gericht. All Rights are reversered.
+   </p>
+ </div>
   </div>
 );
 
